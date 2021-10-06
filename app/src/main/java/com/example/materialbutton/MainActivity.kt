408368d4b5b50aae895585def23478c9c1fb0082
leif2k <leif2k@yandex.ru>
2021-10-06 21:44:34 +0300

    SnackBar

--- app/src/main/java/com/example/materialbutton/MainActivity.kt
+++ app/src/main/java/com/example/materialbutton/MainActivity.kt
@@ -2,18 +2,42 @@ package com.example.materialbutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.CheckBox
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.example.materialbutton.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
 
     private var binding:ActivityMainBinding? =null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
+        binding?.containedIconButton?.setOnClickListener(this)
+
+
+
     }
+
+    override fun onClick(view: View) {
+        // Показывает всплывающее сообщение снизу
+        Snackbar.make(view, R.string.badBoys, Snackbar.LENGTH_LONG)
+
+                // создает кнопку Play на сообщении снизу, и навешивает слушатель нажатий
+            .setAction(R.string.play){
+
+                binding?.result?.text = getString(R.string.startMovies)
+
+            }
+
+            .setBackgroundTint(ContextCompat.getColor (this, R.color.black))
+            .setTextColor(ContextCompat.getColor (this, R.color.white))
+            .setActionTextColor(ContextCompat.getColor (this, R.color.orange))
+            .show()
+    }
+
 }
